537715727dfa27ef5f1697da905f45d85bc3635e
세중 김 <dodrv@naver.com>
2020-11-06 16:22:11 +0900

    CSV Reader 추가

---
+++ Assets/script/utill/CSVReader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public static class csvReader
+{
+    private static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    private static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+    private static char[] TRIM_CHARS = { '\"' };
+
+    public static List<Dictionary<string, string>> Read(string file)
+    {
+        List<Dictionary<string, string>> list = new List<Dictionary<string, string>>();
+        TextAsset data = Resources.Load(file) as TextAsset;
+
+        string[] lines = Regex.Split(data.text, LINE_SPLIT_RE);
+
+        if (lines.Length <= 1) return list;
+
+        var header = Regex.Split(lines[0], SPLIT_RE);
+        for (var i = 1; i < lines.Length; i++)
+        {
+            string[] values = Regex.Split(lines[i], SPLIT_RE);
+            if (values.Length == 0 || values[0] == "")
+                continue;
+
+            Dictionary<string, string> entry = new Dictionary<string, string>();
+            for (var j = 0; j < header.Length && j < values.Length; j++)
+            {
+                string value = values[j];
+                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+
+                value = value.Replace("<br>", "\n"); // 추가된 부분. 개행문자를 \n대신 <br>로 사용한다.
+                value = value.Replace("<c>", ",");
+
+                entry[header[j]] = value;
+            }
+            list.Add(entry);
+        }
+        return list;
+    }
+}
