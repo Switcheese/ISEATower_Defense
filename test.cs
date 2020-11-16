43bef477b3b9f4b112e6ab2bfaedd107f373a652
Switcheese <46314214+Switcheese@users.noreply.github.com>
2020-11-16 12:46:35 +0900

    test2
    222

--- test.cs
+++ test.cs
@@ -7,6 +7,6 @@ public class test : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.name);
-        Debug.Log("TestCommit");
+        Debug.Log("TestCommit2");
     }
 }
