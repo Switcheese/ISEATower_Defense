f459b0851e8490300b64d5684f0e3da6cf2f7277
Switcheese <46314214+Switcheese@users.noreply.github.com>
2020-11-16 12:45:57 +0900

    testcommit
    test

---
+++ test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        Debug.Log("TestCommit");
+    }
+}
